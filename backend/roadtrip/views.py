44481c217dc085d8be22224c95665c72fe1ad9fb
Tobias Cornille <cornilletobias@gmail.com>
2017-10-14 04:09:26 +0200

    Read basic data in

--- backend/roadtrip/views.py
+++ backend/roadtrip/views.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from backend.roadtrip.serializers import UserSerializer, GroupSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from backend.roadtrip.models import Roadtrip
+from backend.roadtrip.serializers import RoadtripSerializer
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+@api_view(['GET', 'POST'])
+def roadtrip_list(request, format=None):
+    if request.method == 'GET':
+        roadtrips = Roadtrip.objects.all()
+        serializer = RoadtripSerializer(roadtrips, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = RoadtripSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/urls.py
+++ backend/urls.py
@@ -18,14 +18,14 @@ from rest_framework import routers
 from backend.roadtrip import views
 from django.contrib import admin
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'roadtrip', views.Roadtrip)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^questions.html/$', views.roadtrip_list),
 ]
