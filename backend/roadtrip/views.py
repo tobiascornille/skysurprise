62db5aee7090216ede8b28506377e93926449539
Tobias Cornille <cornilletobias@gmail.com>
2017-10-14 23:06:23 +0200

    Added consistency

--- backend/roadtrip/views.py
+++ backend/roadtrip/views.py
@@ -72,37 +72,44 @@ class RoadtripData:
     def plan_trip(self):
         day_in_planning = self.inbounddate
         obj_inbounddate = date_string_to_object(self.inbounddate)
-        # find nearest city
+
+        # find nearest city from given (random) location
         self.add_starting_city()
         if len(self.cities) == 0:
             print("couldn't find a trip for you")
             return
-        last_city = self.cities[-1]
 
+        last_city = self.cities[-1]
         # substract the cost to go to this city
         #   is already done in the previous function
 
         # update day_in_planning
-        day_in_planning = calculate_new_date(day_in_planning, days_per_city)
+        day_in_planning = calculate_new_date(day_in_planning, self.days_per_city)
 
-        obj_next_day_in_planning = date_string_to_object(calculate_new_date(day_in_planning))
+        obj_next_day_in_planning = date_string_to_object(calculate_new_date(day_in_planning, self.days_per_city))
 
         # add as much extra cities as possible within the flightbudget,
         # keeping in mind that we also have to get home in time
-        while flightbudget - self.get_flightprice(lastLocation, self.originplace, self.inbounddate) > 0 and obj_next_day_in_planning <= obj_inbounddate:
-            add_city(last_city, day_in_planning, days_per_city)
+        while (self.flightbudget - self.get_flightprice(last_city, self.originplace, self.inbounddate)) > 0 and obj_next_day_in_planning <= obj_inbounddate:
+            self.add_city(last_city, day_in_planning, self.days_per_city)
             last_city = self.cities[-1]
 
-            day_in_planning = calculate_new_date(day_in_planning, days_per_city)
-            obj_day_in_planning = date_string_to_object(day_in_planning)
+            day_in_planning = calculate_new_date(day_in_planning, self.days_per_city)
+            obj_next_day_in_planning = date_string_to_object(day_in_planning)
 
         # terugvlucht moeten aftrekken
-
+        
         # after finding all our locations, we'll have to find a place to stay for the night
 
+    def get_id(self, city_name):
+        resp = requests.get(
+            'http://gateway.skyscanner.net/autosuggest/v3/hotels?q=%s&market=US&locale=en-US&apikey=7772cbd8f1a640ffa9536d96d4c3c48e' % city_name)
+        return resp.json()['results'][0]['id']
+
+
     def add_hotel(self, city, checkindate, checkoutdate, currenthotelnumber):
-        cityid = self.cityids[city]
-        thishotelbudget = self.hotelbudget() / (len(self.cities) - currenthotelnumber)
+        cityid = self.get_id(city)
+        thishotelbudget = self.hotelbudget / (len(self.cities) - currenthotelnumber)
 
         link = "https://gateway.skyscanner.net/hotels/v1/prices/search/entity/{}?market={}&locale={}" \
                "&checkin_date={}&checkout_date={}&currency={}&adults={}&rooms={}&price_max={}&sort={}?apikey={}".format(
@@ -114,9 +121,8 @@ class RoadtripData:
         while hotels.json()["meta"]["status"] != 'COMPLETED':
             hotels = requests.get(link)
 
-        self.hotels.append(hotels["results"])
-
-        print(self.cities)
+        self.hotelbudget -= hotels["offers"][0]
+        self.hotels.append(hotels["results"][0])
 
 
     def add_starting_city(self):
@@ -124,42 +130,41 @@ class RoadtripData:
         radius = 10000
         latitude = self.latitude
         longitude = self.longitude
-        cities = requests.get('http://getnearbycities.geobytes.com/GetNearbyCities?callback=?&latitude={}&longitude={}&'
+        citiesreq = requests.get('http://getnearbycities.geobytes.com/GetNearbyCities?callback=?&latitude={}&longitude={}&'
                               'radius={}&limit={}'.format(latitude, longitude, radius, limit))
-        citiestext = cities.text[2:-2]
+        citiestext = citiesreq.text[2:-2]
         citieslist = eval(citiestext)
 
-
+        end_date = calculate_new_date(self.outbounddate, self.days_per_city)
 
         for city in citieslist:
-            price = self.get_connection_price(self.originplace, city, self.outbounddate, aanpassen)
+            price = self.get_connection_price(self.originplace, city, self.outbounddate, end_date)
             if price != -1:
-                print("HHHHHHHHHHEEEEEEEEEEEEEEEEEEEEYYYYYYYYYYYYY")
                 self.flightbudget -= price
-                self.cities.append[city[1]]
+                self.cities.append(city)
                 return
+
+        print("failed to find a new city. Abort mission")
 
 
     def add_city(self, current_city, start_date, nb_days):
         end_date = calculate_new_date(start_date, nb_days)
-        price = get_connection_price(current_city, "everywhere", start_date, end_date)
+        price = self.get_connection_price(current_city, "everywhere", start_date, end_date)
         if price == -1:
             print("failed to find a new city. Abort mission")
             return
         else:
             self.flightbudget -= price
-            self.cities.append[city[1]]
+            self.cities.append(current_city)
 
     def get_connection_price(self, source, destination, start_date, end_date):
-        print(destination[1])
-        if destination[1] not in self.cities_overview:
-            print("fail")
+        if destination + '\r' not in self.cities_overview:
             return -1
-        print("success")
+
         price = self.get_flightprice(source, destination, start_date)
         returnprice = self.get_flightprice(destination, self.originplace, end_date)
 
-        if price + returnprice < self.budget:
+        if price + returnprice < 1/2 * self.flightbudget: # Half so we have enough budget for the rest of the trip
             return price
 
         return -1
@@ -168,8 +173,8 @@ class RoadtripData:
         link = ("http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/{}/{}/{}/{}/{}/{}/?apikey={}").format(
                 self.country, self.currency, self.locale, source, destination, date, self.apikey
                 )
-        print(link)
         flights = requests.get(link)
+
         price = -1
         max_iterations = 1000
         iteration = 0
