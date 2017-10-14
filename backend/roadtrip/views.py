34becc0f61a593c0a4116908fb81206f35538373
ProLoD <r0668988@kuleuven.be>
2017-10-14 19:14:15 +0200

    trip planning (partial)

--- backend/roadtrip/views.py
+++ backend/roadtrip/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from backend.roadtrip.models import Roadtrip
 from backend.roadtrip.serializers import RoadtripSerializer
 import requests
+from datetime import datetime, timedelta
 
 
 @api_view(['GET', 'POST'])
@@ -41,21 +42,30 @@ class RoadtripData:
         self.flightbudget = budget / 2
         self.rooms = rooms
         self.cities = []
+        self.days_per_city = 3
         self.apikey = 'prtl6749387986743898559646983194'
 
 
     def plan_trip():
+        day_in_planning = self.inbounddate
         # find nearest city
         add_nearest_city(self.originplace)
-        lastLocation = self.cities[-1]
+        last_city = self.cities[-1]
+        if last_city == None:
+            print("couldn't find a trip for you")
+            return
 
         # substract the cost to go to this city
         #   is already done in the previous function
 
+        # update day_in_planning
+        day_in_planning = calculate_new_date(day_in_planning, days_per_city)
+
         # add as much extra cities as possible within the flightbudget,
         # keeping in mind that we also have to get home
         while flightbudget - self.get_flightprice(lastLocation, self.originplace, self.inbounddate) > 0:
-            add_city()
+            add_city(last_city, day_in_planning, days_per_city)
+            last_city = self.cities[-1]
 
         # terugvlucht moeten aftrekken
 
@@ -75,22 +85,28 @@ class RoadtripData:
         citieslist = eval(citiestext)
 
         for city in citieslist:
-            price = self.get_connection_price(self, self.originplace, city, self.outbounddate)
+            price = self.get_connection_price(self, location, city, self.outbounddate, self.inbounddate)
             if price != -1:
                 self.flightbudget -= price
                 self.cities.append[city[1]]
                 return
 
-    def add_city(self, currentcity, date):
-        #TODO
-        # !!hoeveel dagen per stad?
+    def add_city(self, current_city, start_date, nb_days):
+        end_date = calculate_new_date(start_date, nb_days)
+        price = get_connection_price(current_city, "everywhere", start_date, end_date)
+        if price == -1:
+            print("failed to find a new city. Abort mission")
+            return
+        else:
+            self.flightbudget -= price
+            self.cities.append[city[1]]
 
-    def get_connection_price(self, source, destination, date):
+    def get_connection_price(self, source, destination, start_date, end_date):
         if destination not in citiesoverview:
             return -1
 
-        price = self.get_flightprice(source, destination, date)
-        returnprice = self.get_flightprice(destination, self.originplace, self.inbounddate)
+        price = self.get_flightprice(source, destination, start_date)
+        returnprice = self.get_flightprice(destination, self.originplace, end_date)
 
         if price + returnprice < self.budget:
             return price
@@ -103,5 +119,21 @@ class RoadtripData:
                 )
         print(link)
         flights = requests.get(link)
-        price = flights.json()['Routes'][0]['Price']
+        price = -1
+        max_iterations = 1000
+        iteration = 0
+        while (price == -1 and iteration < max_iterations):
+            try:
+                price = flights.json()['Routes'][iteration]['Price']
+            except:
+                price = -1
         return price
+
+    # helper
+    def calculate_new_date(start_date, nb_days):
+        start_date = start_date.split('-')
+        delta_days = timedelta(days=nb_days)
+        end_date = datetime.datetime(year=int(start_date[0], month=int(start_date[1]), day=int(start_date[2])))
+        end_date += delta_days
+        str_end_date = str(end_date.year) + '-' + str(end_date.month) + '-' + str(end_date.day)
+        return str_end_date
